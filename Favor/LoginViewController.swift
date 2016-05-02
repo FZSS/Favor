196dcb2f672ff26ffdcb1ed4a1238e6ef1d9d642
Kevin Fangzhou Chen <kevinchen1219@gmail.com>
2016-05-01 22:29:24 -0700

    fb login working

---
+++ Favor/LoginViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  Favor
+//
+//  Created by Fangzhou Chen on 5/1/16.
+//  Copyright © 2016 Kevin Fangzhou Chen. All rights reserved.
+//
+
+import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+
+class LoginViewController: UIViewController, FBSDKLoginButtonDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let loginButton = FBSDKLoginButton()
+        loginButton.readPermissions = ["email", "public_profile", "user_friends"]
+        view.addSubview(loginButton)
+        loginButton.center = view.center
+        loginButton.delegate = self
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        if FBSDKAccessToken.currentAccessToken() == nil {
+            print("not logged in")
+        } else {
+            print("already logged in")
+            performSegueWithIdentifier("showMainApp", sender: self)
+        }
+    }
+
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+        if error != nil {
+            print(error.localizedDescription)
+            return
+        }
+        print("completed login!")
+        performSegueWithIdentifier("showMainApp", sender: self)
+        
+    }
+    
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
+    }
+    
+    func loginButtonWillLogin(loginButton: FBSDKLoginButton!) -> Bool {
+        return true
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
