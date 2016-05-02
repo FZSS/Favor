fc80b8d1a3049b80cdb90e9ff5810dfee0839c66
Kevin Fangzhou Chen <kevinchen1219@gmail.com>
2016-05-02 04:07:15 -0700

    table view

---
+++ Favor/FavorTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FavorTableViewCell.swift
+//  Favor
+//
+//  Created by Fangzhou Chen on 5/2/16.
+//  Copyright © 2016 Kevin Fangzhou Chen. All rights reserved.
+//
+
+import UIKit
+
+class FavorTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
