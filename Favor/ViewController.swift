1cfd3184ea524bb80540ae8d9526cdea11f279ef
Kevin Fangzhou Chen <kevinchen1219@gmail.com>
2016-05-01 20:46:31 -0700

    Initial Commit

---
+++ Favor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Favor
+//
+//  Created by Fangzhou Chen on 5/1/16.
+//  Copyright © 2016 Kevin Fangzhou Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
